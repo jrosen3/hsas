401cba580aa8d6a69d959cef3db1e28380c2cb06
Jared Rosen <jaredarosen@gmail.com>
2013-12-05 23:14:55 -0500

    update

--- production/js/woahbar.js
+++ production/js/woahbar.js
@@ -10,7 +10,17 @@ function woahbar_show() {
     else {
       $('.woahbar').show('bounce', { times:3, distance:15 }, 100); 
       $('body').animate({"marginTop": "2.4em"}, 250);
-    }
+    };
+
+    // move button down
+    $(".bt-menu-trigger").css({
+      'top':'40px'
+    });
+
+    $(".bt-menu ul:first-of-type").css({
+      'top':'100px'
+    });
+
 }
 
 function woahbar_hide() { 
@@ -21,7 +31,18 @@ function woahbar_hide() {
 
     if( $(window).width() > 1024 ) {
       $('body').animate({"marginTop": "0px"}, 250); // if width greater than 1024 pull up the body
-    }
+    };
+
+    // move button up
+    $(".bt-menu-trigger").css({
+      'top':'10px'
+    });
+
+
+    $(".bt-menu ul:first-of-type").css({
+      'top':'70px'
+    });
+
 }
 
 $().ready(function() {
